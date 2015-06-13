cffd77cdc3821719de2988a78240d8d2330ab44f
Conlan McNamee <conlanmcnamee@gmail.com>
2015-06-12 20:08:56 -0700

    what we did initially in class

---
+++ app/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$(function() {
+	$("#name").on('click', function() {
+		$.get("http://localhost:3000/adjective", function(response) {
+			var adjective = response.word;
+			$('#adjective').text(adjective);
+		});
+	});
+});
