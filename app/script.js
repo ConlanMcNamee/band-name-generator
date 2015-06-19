d3e937328be1ac3da895f61e2f4c1c2352616c47
Conlan McNamee <conlanmcnamee@gmail.com>
2015-06-18 19:15:26 -0700

    added my procfile

--- app/script.js
+++ app/script.js
@@ -6,5 +6,44 @@ $(function() {
 			var adjective = response.word;
 			$('#adjective').text(adjective);
 		});
+		$.get("http://localhost:3000/verb", function(response) {
+			var verb = response.word;
+			$('#verb').text(verb);
+		});
+		$.get("http://localhost:3000/noun", function(response) {
+			var noun = response.word;
+			$('#noun').text(noun);
+		});
+	});
+	$('#submitWords').on('submit', function(e) {
+		e.preventDefault();
+		var adjective = $("[name=adjective]").val();
+		var adjPost;
+		var verb = $("[name=verb]").val();
+		var verbPost;
+		var noun = $("[name=noun]").val();
+		var nounPost;
+
+		if(adjective) {
+			adjPost = {word: adjective};
+			$.post("adjective", adjPost, function(response) {
+				var adjectiveRes = response.msg;
+				$("#adjectiveRes").text(adjectiveRes);
+			});
+		}
+		if(verb) {
+			verbPost = {word: verb};
+			$.post("verb", verbPost, function(response) {
+				var verbRes = response.msg;
+				$("#verbRes").text(verRes);
+			});
+		}
+		if(noun) {
+			nounPost = {word: noun};
+			$.post("noun", nounPost, function(response) {
+				var nounRes = response.msg;
+				$("#nounRes").text(nounRes);
+			});
+		}
 	});
 });
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require("express");
+var bodyparser = require("body-parser");
+var Adjective = require("./lib/adjective.js");
+var Verb = require("./lib/verb.js");
+var Noun = require("./lib/noun.js");
+var getRandomWord = require("./lib/getRandomWord.js");
+var postRandomWord = require("./lib/postRandomWord.js");
 var app = express();
 var port = process.env.PORT || 3000;
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 
 app.listen(port, function() {
 	console.log('server available at http://localhost:' + port);
@@ -8,24 +17,49 @@ app.listen(port, function() {
 
 app.use(express.static(__dirname + '/app/'));
 
-var Adjective = function() {
- this.sleepy = true;
- this.fuzzy = true;
- this.cranky = true;
- this.soporific = true;
-this.eloquent = true;
-};
+var verb = new Verb();
 var adjective = new Adjective();
-function getRandomWord (object) {
- var propArray = Object.keys(object);
- var randomProp = propArray[Math.floor(Math.random() * propArray.length)];
- return {word: randomProp};
-}
+var noun = new Noun();
 
-app.get('/adjective', function (req, res) {
-	res.json(getRandomWord(adjective));
-});
+
 
 app.get("/", function (req, res) {
 	res.sendFile("index.html");
 });
+app.get('/adjective', function (req, res) {
+	res.json(getRandomWord(adjective));
+});
+app.get('/verb', function (req,res) {
+	res.json(getRandomWord(verb));
+});
+app.get('/noun', function (req,res) {
+	res.json(getRandomWord(noun));
+});
+
+app.post('/adjective', function (req,res) {
+	var word = postRandomWord(req.body.word, adjective);
+	res.json(word);
+});
+
+app.post('/verb', function (req,res) {
+	var word = postRandomWord(req.body.word, verb);
+	res.json(word);
+});
+app.post('/noun', function (req,res) {
+	var word = postRandomWord(req.body.word, noun);
+	res.json(word);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
